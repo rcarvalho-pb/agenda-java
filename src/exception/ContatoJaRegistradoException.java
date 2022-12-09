fd493c97a635c454bfb7b075860a9a603b505f4c
hgrafa <hugorafell@gmail.com>
2022-12-09 09:02:59 -0300

    Adding ContatoJaRegistrado exception

---
+++ src/exception/ContatoJaRegistradoException.java
@@ -0,0 +1,12 @@
+package exception;
+
+public class ContatoJaRegistradoException extends RuntimeException {
+
+  public ContatoJaRegistradoException() {
+  }
+
+  public ContatoJaRegistradoException(String nomeContato) {
+    super("O contato " + nomeContato + " já está registrado!");
+  }
+
+}
