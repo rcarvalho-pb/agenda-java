e67c1467d85a5278cfd97e5477920950cc748039
aasnetoo <115680563+aasnetoo@users.noreply.github.com>
2022-12-12 15:09:58 -0300

    atualizacao

--- src/model/Contato.java
+++ src/model/Contato.java
@@ -10,10 +10,6 @@ public class Contato {
     private List<Endereco> enderecos;
     private String email;
 
-    public Contato(String nome, String email) {
-        this.nome = nome;
-        this.email = email;
-    }
 
     public Contato(String nome, String sobreNome, String email) {
         this.nome = nome;
